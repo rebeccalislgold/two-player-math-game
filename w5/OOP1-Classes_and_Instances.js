7b53ed097cf991aebbc14e968be487954b995de9
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-05-29 04:16:14 +0000

    Added getters and setters

--- w5/OOP1-Classes_and_Instances.js
+++ w5/OOP1-Classes_and_Instances.js
@@ -1,7 +1,7 @@
 class Pizza {
 
     constructor(size, crust, shape) {
-      this.size = size;
+      this._size = size;
       this.crust = crust;
       this.shape = shape;
       this.toppings = ["cheese"];
@@ -10,19 +10,50 @@ class Pizza {
     addTopping(topping) {
       this.toppings.push(topping);
     }
+
+    get price() {
+      const basePrice = 10;
+      const toppingPrice = 2;
+      return basePrice + this.toppings.length * toppingPrice;
+    }
+  
+    set size(size) {
+      if (size === 's' || size === 'm' || size === 'l') {
+        this._size = size;
+      }
+    }
+
+    get size() {
+      return this._size;
+    }
   
   }
+  
 
-let pizza1 = new Pizza('large', 'thin', 'square');
-console.log(pizza1.toppings); // ["cheese"]
-pizza1.addTopping("mushrooms");
-pizza1.addTopping("peppers");
-console.log(pizza1.toppings); // ["cheese", "mushrooms", "peppers"]
+// let pizza1 = new Pizza('large', 'thin', 'square');
+// console.log(pizza1.toppings); // ["cheese"]
+// pizza1.addTopping("mushrooms");
+// pizza1.addTopping("peppers");
+// console.log(pizza1.toppings); // ["cheese", "mushrooms", "peppers"]
 
-// let pizza2 = new Pizza();
-// console.log(pizza2.toppings); // ["cheese"]
-// pizza2.addTopping("more cheese");
-// console.log(pizza2.toppings); // ["cheese", "more cheese"];
+// // let pizza2 = new Pizza();
+// // console.log(pizza2.toppings); // ["cheese"]
+// // pizza2.addTopping("more cheese");
+// // console.log(pizza2.toppings); // ["cheese", "more cheese"];
 
-console.log("PIZZA1:", pizza1);
+// console.log("PIZZA1:", pizza1);
 // console.log("PIZZA2:", pizza2);
+
+// let pizza = new Pizza('SUPER L', 'stuffed-crust', 'square');
+// pizza.setSize('s');
+
+// // console.log(pizza);
+// console.log(pizza.getSize());
+
+let pizza = new Pizza();
+
+pizza.price;      // instead of getPrice()
+pizza.size = 's'; // instead of setSize(size)
+
+console.log(`PRICE: ${pizza.price}, SIZE: ${pizza.size}`);
+console.log(`PIZZA TOPPINGS: ${pizza.toppings}`);
